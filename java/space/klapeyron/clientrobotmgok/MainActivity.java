3b4aeae841e586dcd0adb2d46bdfc91be5966d17
Klapeyron <Nikitoslav-razdol@ya.ru>
2016-04-11 10:13:22 +0300

    after auto commit cleaning

--- java/space/klapeyron/clientrobotmgok/MainActivity.java
+++ java/space/klapeyron/clientrobotmgok/MainActivity.java
@@ -359,11 +359,7 @@ public class MainActivity extends Activity {
     private void connectMethod() {
         setClientState(CLIENT_CONNECTING);
         try {
-<<<<<<< HEAD
             if (selectedServer.getName().equals(ROBOT_SERVER_NAME))/*&&(selectedServer.getAddress().equals(ROBOT_SERVER_ADRESS)))*/ {
-=======
-            if ((selectedServer.getName().equals(ROBOT_SERVER_NAME))){//&&(selectedServer.getAddress().equals(ROBOT_SERVER_ADRESS))) {
->>>>>>> origin/master
                 serverSocket = selectedServer.createRfcommSocketToServiceRecord(java.util.UUID.fromString(UUID));
                 Log.i("TAG", "try to connect");
                 serverSocket.connect(); //send connection request to server, and server send back the confirmation, what server is ready
